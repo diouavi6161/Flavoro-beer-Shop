ff88e849b598e878427448e7404b362d80a4a727
avi <avilinid035@gmail.com>
2024-06-12 11:18:52 +0530

    update helper/js

--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
-export const getCart = async (user)=>{
-    const res = await axios.get(`http://localhost:5001/api/get-cart/${user._id}`)
-    const data = await res.data
-    return data
-}
+export const getCart = async (user) => {
+  if (!user || !user._id) {
+    throw new Error("User is not defined or _id is missing");
+  }
+  const res = await axios.get(`https://beer-shop-backend-1.onrender.com/api/get-cart/${user._id}`);
+  const data = await res.data;
+  return data;
+};
